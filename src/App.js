6753c502c888893458db82d4460bc82f84ae2935
Jarret Logan <jarret.logan@publicissapient.com>
2020-01-09 14:35:08 -0600

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import { Route } from 'react-router-dom';
 
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Route exact path='/' component={HomePage} />
+    </div>
+  );
 }
 
 export default App;
